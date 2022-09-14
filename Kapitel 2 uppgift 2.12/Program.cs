545c3e54734cc866ddf4dbb8153e1546b549c8fe
Admin <Admin@TEB123021>
2022-09-14 12:01:59 +0200

    Add project files.

---
+++ Kapitel 2 uppgift 2.12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace uppgift2_12
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hur länge har du tänkt hyra bilen?");
+            string strDagartext = Console.ReadLine();   
+            int Dagar = int.Parse(strDagartext);
+
+            
+            Console.WriteLine("Hur många KM du tänkt köra?"); 
+            string strKMtext=Console.ReadLine();
+            int KM = int.Parse(strKMtext);
+
+            int total; 
+            total = 300 + 500 * (Dagar - 1) + KM;
+            Console.WriteLine("Den totala summan blir " + total  ); 
+   
+
+        }
+
+
+    }
+  
+}
